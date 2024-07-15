323a27796f00c99230fbbd7fb1844bc5ce03971c
Desmond Yentumi <yentumid7@gmail.com>
2024-07-15 16:19:56 +0000

    validated the post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 4, maximum: 20}
+    validates :description, presence: true, length: {minimum: 4, maximum: 50}
+    validates :keywords, presence: true, length: {minimum: 4, maximum: 20}
 end
