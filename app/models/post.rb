b64aaaca016e62f763b3cbf3ba7c5e0917aa25fb
Desmond Yentumi <yentumid7@gmail.com>
2024-07-15 17:19:51 +0000

    Added some images to my page

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 4, maximum: 20}
     validates :description, presence: true, length: {minimum: 4, maximum: 50}
     validates :keywords, presence: true, length: {minimum: 4, maximum: 20}
+    
+    has_many_attached :images
 end
