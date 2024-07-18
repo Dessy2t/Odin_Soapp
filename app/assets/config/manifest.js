51519d9453ce57ed400253e49dc674e925b33e2d
Desmond Yentumi <yentumid7@gmail.com>
2024-07-18 09:27:57 +0000

    Done

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-//= link post..css
+//= link navbar.css
+//= link post.css
+//= link application.css
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,8 +13,8 @@ class Post < ApplicationRecord
     before_create :randomize_id
     private
     def randomize_id
-        begin
-          self.id = SecureRandom.random_number(1_000_000_000)
-        end while User.where(id: self.id).exists?
+      begin
+        self.id = SecureRandom.random_number(1_000_000_000)
+      end while User.where(id: self.id).exists?
     end
 end
